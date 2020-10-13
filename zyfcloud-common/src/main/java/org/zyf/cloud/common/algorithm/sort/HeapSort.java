bbb8ad7d46955ed0adadbf459b46f3f977eee781
zhangyanfeng04 <zhangyanfeng04@meituan.com>
2020-10-13 14:33:16 +0800

    调整文本

--- zyfcloud-common/src/main/java/org/zyf/cloud/common/algorithm/sort/HeapSort.java
+++ zyfcloud-common/src/main/java/org/zyf/cloud/common/algorithm/sort/HeapSort.java
@@ -14,14 +14,14 @@ public class HeapSort {
      * @return int[]
      */
     public static int[] heapSort(int[] array) {
-        // 初始建堆，array[0]为第一趟值最大的元素
+        /*初始建堆，array[0]为第一趟值最大的元素*/
         array = buildMaxHeap(array);
         for (int i = array.length - 1; i > 1; i--) {
-            // 将堆顶元素和堆低元素交换，即得到当前最大元素正确的排序位置
+            /* 将堆顶元素和堆低元素交换，即得到当前最大元素正确的排序位置*/
             int temp = array[0];
             array[0] = array[i];
             array[i] = temp;
-            // 整理，将剩余的元素整理成堆
+            /*整理，将剩余的元素整理成堆*/
             adjustDownToUp(array, 0, i);
         }
         return array;
@@ -34,7 +34,7 @@ public class HeapSort {
      * @return int[]
      */
     private static int[] buildMaxHeap(int[] array) {
-        // 从最后一个节点array.length-1的父节点（array.length-1-1）/2开始，直到根节点0，反复调整堆
+        /* 从最后一个节点array.length-1的父节点（array.length-1-1）/2开始，直到根节点0，反复调整堆 */
         for (int i = (array.length - 2) / 2; i >= 0; i--) {
             adjustDownToUp(array, i, array.length);
         }
@@ -49,23 +49,23 @@ public class HeapSort {
     private static void adjustDownToUp(int[] array, int k, int length) {
         int temp = array[k];
         for (int i = 2 * k + 1; i < length - 1; i = 2 * i + 1) {
-            // i为初始化为节点k的左孩子，沿节点较大的子节点向下调整
+            /* i为初始化为节点k的左孩子，沿节点较大的子节点向下调整*/
             if (i < length && array[i] < array[i + 1]) {
-                // 取节点较大的子节点的下标
-                i++; // 如果节点的右孩子>左孩子，则取右孩子节点的下标
+                /* 取节点较大的子节点的下标, 如果节点的右孩子>左孩子，则取右孩子节点的下标*/
+                i++;
             }
             if (temp >= array[i]) {
-                // 根节点 >=左右子女中关键字较大者，调整结束
+                /* 根节点 >=左右子女中关键字较大者，调整结束*/
                 break;
             } else {
-                // 根节点 <左右子女中关键字较大者
-                // 将左右子结点中较大值array[i]调整到双亲节点上
+                /* 根节点 <左右子女中关键字较大者
+                 将左右子结点中较大值array[i]调整到双亲节点上*/
                 array[k] = array[i];
-                //【关键】修改k值，以便继续向下调整
+                /*【关键】修改k值，以便继续向下调整 */
                 k = i;
             }
         }
-        // 被调整的结点的值放人最终位置
+        /* 被调整的结点的值放人最终位置*/
         array[k] = temp;
     }
 
@@ -76,10 +76,10 @@ public class HeapSort {
      * @return int[]
      */
     private static int[] deleteMax(int[] array) {
-        // 将堆的最后一个元素与堆顶元素交换，堆底元素值设为-99999
+        /* 将堆的最后一个元素与堆顶元素交换，堆底元素值设为-99999*/
         array[0] = array[array.length - 1];
         array[array.length - 1] = -99999;
-        // 对此时的根节点进行向下调整
+        /* 对此时的根节点进行向下调整*/
         adjustDownToUp(array, 0, array.length);
         return array;
     }
@@ -91,25 +91,25 @@ public class HeapSort {
      * @return int[]
      */
     private static int[] insertData(int[] array, int data) {
-        /**将新节点放在堆的末端*/
+        /*将新节点放在堆的末端*/
         array[array.length - 1] = data;
-        /**需要调整的节点*/
+        /*需要调整的节点*/
         int k = array.length - 1;
-        /**双亲节点*/
+        /*双亲节点*/
         int parent = (k - 1) / 2;
         while (parent >= 0 && data > array[parent]) {
-            /**双亲节点下调*/
+            /*双亲节点下调*/
             array[k] = array[parent];
             k = parent;
             if (parent != 0) {
-                /**继续向上比较*/
+                /*继续向上比较*/
                 parent = (parent - 1) / 2;
             } else {
-                /**根节点已调整完毕，跳出循环*/
+                /*根节点已调整完毕，跳出循环*/
                 break;
             }
         }
-        /**将插入的结点放到正确的位置*/
+        /*将插入的结点放到正确的位置*/
         array[k] = data;
         return array;
     }
